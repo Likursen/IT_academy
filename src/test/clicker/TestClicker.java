c12a7913f9d2bfd118c5fba4b39df3d9ed262526
Likursen <dimak124578@gmail.com>
2022-04-11 22:02:01 +0300

    add class Clicker

---
+++ src/test/clicker/TestClicker.java
@@ -0,0 +1,15 @@
+package test.clicker;
+
+public class TestClicker {
+    public static void main(String[] args) throws InterruptedException {
+        Clicker clicker = new Clicker();
+        clicker.start();
+
+        Thread.sleep(50);
+
+        clicker.stopClicking();
+        clicker.join();
+
+        System.out.println("Finish!" + clicker.getClick());
+    }
+}
